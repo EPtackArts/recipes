7e05b55f6266e65b0f8c844671f5165d713ebd94
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2023-04-06 20:35:46 -0500

    firebase initialized

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,7 @@
-import {initializeApp} from "firebase/app";
-import {getAuth, onAuthStateChanged} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import { config } from 'dotenv';
 
-const firebaseApp = initializeApp({
-    apiKey: "AIzaSyBtkS_ajPgo6FzsUlvgWiv3_-_hKyRfils",
-    authDomain: "recipes-fd3f9.firebaseapp.com",
-    projectId: "recipes-fd3f9",
-    storageBucket: "recipes-fd3f9.appspot.com",
-    messagingSenderId: "1016825570140",
-    appId: "1:1016825570140:web:58db148c554680e8be5799",
-    measurementId: "G-R52YQMTHGE"
-});
-const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
+config();
 
-//Delete auth state
-onAuthStateChanged(auth, user => {
-    if(user != null) {
-        console.log("logged in!");
-    } else {
-        console.log("no user!");
-    }
-});
+import firebaseSetup from "./api/firebase.js";
+firebaseSetup();
+console.log("Firebase initialized!");
